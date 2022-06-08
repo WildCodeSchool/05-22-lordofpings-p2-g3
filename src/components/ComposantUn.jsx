a8645e159cc18653190a5f0e6e04547e3254aebd
victor DG <v.degermain@gmail.com>
2022-06-08 23:04:43 +0200

    test react lifleft

--- src/components/ComposantUn.jsx
+++ src/components/ComposantUn.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { MapContainer, TileLayer, useMap,Marker,Popup } from 'react-leaflet'
 
 const ComposantUn = () => {
+  const position = [51.505, -0.09]
+
+
+
   return (
     <div>
         
        <h1>ComposantUn incrustation dans Composant MAIN</h1> 
+
+         <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={position}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  </MapContainer>
         
     </div>
   )
