0f33f1e82608f3489439cfeed49eaeacb3d9313f
victor DG <v.degermain@gmail.com>
2022-06-09 18:12:47 +0200

    ajout logo et animation pour burger menu

--- src/components/templates/Header.jsx
+++ src/components/templates/Header.jsx
@@ -1,10 +1,14 @@
 import './Header.css'
-
+import logo from '../../assets/images/rockYourBand-transparent.png';
 
 const Header = () => {
 
     return (
     <header className="header d-flex justify-content-center p-20">   
+          
+                <img className="logo" src={logo} />
+       
+            
             <h1> 🎤 🎧 🎼 🎹 🎷 ROCK YOUR BAND  🎺 🎸 🎻</h1>
     </header>
     )      
