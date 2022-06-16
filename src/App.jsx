7aa8d974534d40675ba667e99b8504b2312e64e8
Bricius13 <charifibrice.pro@gmail.com>
2022-06-16 10:52:24 +0200

    acceptation changement app02

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import './App.css'
-import React,{useState}from 'react'
+import React, { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 import Home from './screen/Home'
 import Annuaire from './screen/Annuaire'
 import Evenements from './screen/Evenements'
 import Apropos from './screen/Apropos'
-<<<<<<< HEAD
-import Annuaire from './screen/Annuaire'
-=======
 import Header from './components/templates/Header'
 import Footer from './components/templates/Footer'
 import Main from './components/templates/Main'
->>>>>>> dev
 
 function App() {
   const [isHomePage, setIsHomePage] = useState(true)
@@ -22,9 +18,15 @@ function App() {
       <Header className='header' isHomePage={isHomePage}></Header>
       <Main>
         <Routes>
-          <Route path='/' element={<Home setIsHomePage={setIsHomePage}/>} />
-          <Route path='/annuaire' element={<Annuaire setIsHomePage={setIsHomePage} />} />
-          <Route path='/evenement' element={<Evenements setIsHomePage={setIsHomePage} />} />
+          <Route path='/' element={<Home setIsHomePage={setIsHomePage} />} />
+          <Route
+            path='/annuaire'
+            element={<Annuaire setIsHomePage={setIsHomePage} />}
+          />
+          <Route
+            path='/evenement'
+            element={<Evenements setIsHomePage={setIsHomePage} />}
+          />
           <Route path='/about' element={<Apropos />} />
         </Routes>
       </Main>
