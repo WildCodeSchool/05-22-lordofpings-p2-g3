7795125ba18ad7b7169b3fc06483499126b98454
victor DG <v.degermain@gmail.com>
2022-06-18 13:21:13 +0200

    graphql request and stringyfy

--- src/screen/Graphqlpage.jsx
+++ src/screen/Graphqlpage.jsx
@@ -1,37 +1,70 @@
 import React, { useEffect, useState } from 'react'
 import { gql, useQuery } from '@apollo/client'
 
+const GET_LOCATIONS = gql`
+  query location {
+    locations {
+      name
+      location
+    }
+  }
+`
+
+const GET_MUSICIAN = gql`
+  query musicien {
+    musican {
+      status
+      instruments
+      id
+      status
+    }
+  }
+`
+
+const GET_FILE = gql`
+  query files {
+    files {
+      filename_disk
+    }
+  }
+`
+const GET_ARTICLES = gql`
+query articles {
+  articles {
+    id
+    status
+    author {
+      email
+      location
+      password
+      first_name
+      avatar
+    }
+  }
+}`
+
 const GraphqlPage = ({ setIsHomePage }) => {
+  // const [musicien, setMusicien] = useState({})
+
   useEffect(() => {
     return setIsHomePage(false)
   }, [])
 
-  const { data } = useQuery(gql`
-    query location{
-      locations {
-        name
-        location
-      }
-    }
-  `)
-  // const { data } = useQuery(gql`
-  //   query musicien {
-  //     Musican {
-  //       status
-  //       instruments
-  //       id
-  //       status
-  //     }
-  //   }
-  // `)
-    console.log(data)
+  const { loading, error, data } = useQuery(GET_ARTICLES)
+  // useEffect(() => {
+  //   { loading, error, data }=
+  // }, [])
 
+  if (loading) return 'Loading...'
+  if (error) return `Error! ${error.message}`
+
+  console.log(data)
 
   return (
     <div className='main'>
       <div className='container-80'>
         <h1>Page GRAPHQL</h1>
-        {JSON.stringify(data)}
+        <pre>{JSON.stringify(data, null, 2)}</pre>
       </div>
     </div>
   )
