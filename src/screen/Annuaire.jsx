2a78a9ede8c141bef37178baf42aeb4d938f7cb0
Jonas Moreau <jonas.moreau@hotmail.com>
2022-07-04 17:23:10 +0200

    map groupe

--- src/screen/Annuaire.jsx
+++ src/screen/Annuaire.jsx
@@ -137,22 +137,22 @@ const Annuaire = ({ setIsHomePage }) => {
                     url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
                   />
 
-                  {!noCreteria && profilesFilter.length
-                    ? profilesFilter.map(profile => (
-                        <Leaflet
-                          key={profile.id}
-                          id={profile.id}
-                          name={profile.name.first}
-                          image={profile.picture.large}
-                          location={profile.location.city}
-                          instrument={profile.music.instrument}
-                          experience={profile.music.experience}
-                          style={profile.music.style}
-                          latitude={profile.location.coordinates.latitude}
-                          longitude={profile.location.coordinates.longitude}
-                        />
-                      ))
-                    : deso && <p>deso gros</p>}
+                  {!noCreteria &&
+                    profilesFilter.length &&
+                    profilesFilter.map(profile => (
+                      <Leaflet
+                        key={profile.id}
+                        id={profile.id}
+                        name={profile.name.first}
+                        image={profile.picture.large}
+                        location={profile.location.city}
+                        instrument={profile.music.instrument}
+                        experience={profile.music.experience}
+                        style={profile.music.style}
+                        latitude={profile.location.coordinates.latitude}
+                        longitude={profile.location.coordinates.longitude}
+                      />
+                    ))}
 
                   {noCreteria &&
                     profiles !== null &&
@@ -179,11 +179,11 @@ const Annuaire = ({ setIsHomePage }) => {
       <div className='title2'>
         <h3>Ou votre futur groupe de musique ... </h3>
       </div>
-      <div className='containerGroupe'>
-        {group.map(
-          (group, index) =>
-            index < 11 && (
-              <div className='containerGroupe'>
+      <div className='elment-annuaire'>
+        <div className='containerSolo'>
+          {group.map(
+            (group, index) =>
+              index < 11 && (
                 <Profiles
                   key={group.id}
                   id={group.id}
@@ -193,9 +193,45 @@ const Annuaire = ({ setIsHomePage }) => {
                   instrument={group.instrument}
                   experience={group.experience}
                 />
-              </div>
-            )
-        )}
+              )
+          )}
+        </div>
+        <div>
+          <>
+            <div className='wrap-leaf'>
+              {
+                <MapContainer
+                  center={[49.837965, 6.057441]}
+                  zoom={5}
+                  scrollWheelZoom={false}
+                >
+                  <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                  />
+                  {group.map(
+                    (group, index) =>
+                      index < 11 && (
+                        <div className='containerGroupe'>
+                          <Leaflet
+                            key={group.id}
+                            id={group.id}
+                            name={group.name}
+                            image={group.jacket}
+                            location={group.location.city}
+                            instrument={group.instrument}
+                            experience={group.experience}
+                            latitude={group.location.coordinates.latitude}
+                            longitude={group.location.coordinates.longitude}
+                          />
+                        </div>
+                      )
+                  )}
+                </MapContainer>
+              }
+            </div>
+          </>
+        </div>
       </div>
     </div>
   )
