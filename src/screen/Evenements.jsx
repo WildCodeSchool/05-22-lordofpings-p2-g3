9861c6a196e3aee8ef633765ea0f2af34245619f
Jonas Moreau <jonas.moreau@hotmail.com>
2022-07-06 20:50:16 +0200

    leaflet evenements + intégration css avec card events

--- src/screen/Evenements.jsx
+++ src/screen/Evenements.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import Events from '../components/Events'
-import { Leaflet } from '../components/Leaflet'
+import { MapContainer, TileLayer } from 'react-leaflet'
+import { LeafletEvents } from '../components/LeafletEvents'
 import './Evenements.css'
 
 const Evenements = ({ setIsHomePage }) => {
@@ -115,43 +116,95 @@ const Evenements = ({ setIsHomePage }) => {
           </button>
         </div>
       </form>
-      <div className='GaleryEvenements'>
-        {!noCreteria && evenementsF.length
-          ? evenementsF.map(evenement => (
+      <div className='elment-annuaire'>
+        <div className='containerSolo'>
+          {!noCreteria && evenementsF.length
+            ? evenementsF.map(evenement => (
+                <Events
+                  key={evenement.id}
+                  name={evenement.name}
+                  image={evenement.image}
+                  description={evenement.description}
+                  id={evenement.id}
+                  url={evenement.url}
+                  tarif={evenement.tarif}
+                  style={evenement.style}
+                  location={evenement.location}
+                  city={evenement.city}
+                  date_start={evenement.date_start}
+                  infos={evenement.info_pratique}
+                />
+              ))
+            : noResult && <p className='noResultAff'>Aucun résultat</p>}
+          {noCreteria &&
+            evenements !== null &&
+            evenements.map(evenementF => (
               <Events
-                key={evenement.id}
-                name={evenement.name}
-                image={evenement.image}
-                description={evenement.description}
-                id={evenement.id}
-                url={evenement.url}
-                tarif={evenement.tarif}
-                style={evenement.style}
-                location={evenement.location}
-                city={evenement.city}
-                date_start={evenement.date_start}
-                infos={evenement.info_pratique}
+                key={evenementF.id}
+                name={evenementF.name}
+                image={evenementF.image}
+                description={evenementF.description}
+                id={evenementF.id}
+                url={evenementF.url}
+                tarif={evenementF.tarif}
+                style={evenementF.style}
+                location={evenementF.location}
+                city={evenementF.city}
+                date_start={evenementF.date_start}
+                infos={evenementF.info_pratique}
               />
-            ))
-          : noResult && <p className='noResultAff'>Aucun résultat</p>}
-        {noCreteria &&
-          evenements !== null &&
-          evenements.map(evenementF => (
-            <Events
-              key={evenementF.id}
-              name={evenementF.name}
-              image={evenementF.image}
-              description={evenementF.description}
-              id={evenementF.id}
-              url={evenementF.url}
-              tarif={evenementF.tarif}
-              style={evenementF.style}
-              location={evenementF.location}
-              city={evenementF.city}
-              date_start={evenementF.date_start}
-              infos={evenementF.info_pratique}
-            />
-          ))}
+            ))}
+        </div>
+
+        <div>
+          <div className='wrap-leaf'>
+            {
+              <MapContainer
+                center={[47.146135, 2.325562]}
+                zoom={6}
+                scrollWheelZoom={false}
+              >
+                <TileLayer
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                  url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                />
+                {!noCreteria && evenementsF.length
+                  ? evenementsF.map(evenement => (
+                      <LeafletEvents
+                        key={evenement.id}
+                        name={evenement.name}
+                        image={evenement.image}
+                        id={evenement.id}
+                        url={evenement.url}
+                        tarif={evenement.tarif}
+                        style={evenement.style}
+                        city={evenement.city}
+                        date_start={evenement.date_start}
+                        coordinates={evenement.location.coordinates}
+                      />
+                    ))
+                  : noResult && <p className='noResultAff'>Aucun résultat</p>}
+
+                {noCreteria &&
+                  evenements !== null &&
+                  evenements.map(evenementF => (
+                    <LeafletEvents
+                      key={evenementF.id}
+                      name={evenementF.name}
+                      image={evenementF.image}
+                      id={evenementF.id}
+                      url={evenementF.url}
+                      tarif={evenementF.tarif}
+                      style={evenementF.style}
+                      city={evenementF.city}
+                      date_start={evenementF.date_start}
+                      coordinates={evenementF.location.coordinates}
+                    />
+                  ))}
+              </MapContainer>
+            }
+          </div>
+        </div>
       </div>
     </>
   )
